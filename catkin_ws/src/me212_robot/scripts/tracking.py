b0ca43f2b10df8251936850774161dd56915871b
goldflower <aabb0852@hotmail.com>
2017-10-03 00:41:47 +0800

    task2 and task3

--- catkin_ws/src/me212_robot/scripts/tracking.py
+++ catkin_ws/src/me212_robot/scripts/tracking.py
@@ -4,6 +4,7 @@ from std_msgs.msg import Float64MultiArray # ros的message的資料類型
 from math import pi, radians
 class Tracking:
     def __init__(self):
+        self.initial_wheel_pwm = 60
         # node上運行此class, get the fully-qualified name of this node, 在此即為'tracking'
         self.node_name = rospy.get_name() 
         self.state = 1 # ?
@@ -12,8 +13,8 @@ class Tracking:
         self.motorhat = Adafruit_MotorHAT(addr= 0x60)
         self.leftMotor     = self.motorhat.getMotor(1) # between 1-4
         self.rightMotor = self.motorhat.getMotor(2)
-        self.right_pwm = 60
-        self.left_pwm = 60
+        self.right_pwm = self.initial_wheel_pwm
+        self.left_pwm = self.initial_wheel_pwm
         self.leftMotor.setSpeed(self.left_pwm) # 0-255
         self.rightMotor.setSpeed(self.right_pwm)
         # subscriber去訂閱topic "/serial_node/odometry"(graph resource name of topic), 
@@ -26,7 +27,7 @@ class Tracking:
         self.RELEASE = 4
         self.adjust_pwm = 5 # ?
         # 決定第一次進入stage的行為, 可能用不到
-        self.in_stage1 = self.in_stage2 = self.in_stage3 = False
+        self.count_in_stage1 = self.count_in_stage2 = self.count_in_stage3 = 0
         rospy.on_shutdown(self.custom_shutdown)
         rospy.loginfo("[%s] Initialized!" %self.node_name)
 
@@ -37,29 +38,36 @@ class Tracking:
         theta = theta % (2* pi)
         print(x,y,theta)
 
-        if x <=0: # stright
-            self.in_stage1 = True
+        if x <= 0: # stright
+            if theta > 0: # left_pwm++ or right_pwm--
+                self.right_pwm -= 3
+            elif theta < 0:
+                self.left_pwm -= 3
             self.leftMotor.setSpeed(self.left_pwm)
             self.rightMotor.setSpeed(self.left_pwm)
             self.leftMotor.run(self.FORWARD)
             self.rightMotor.run(self.FORWARD)
         elif x >= 1 and theta < pi: # semi-circle
-            if not self.in_stage2:
-                self.in_stage2 = True
-                self.right_pwm = 120
-                self.left_pwm = 60 
+            if self.count_in_stage2 == 0: # 不知道有沒有用的heuristic, 先調整到正確的pwm走一下
+                self.count_in_stage2 += 1
+                self.leftMotor.run(self.BRAKE)
+                self.rightMotor.run(self.BRAKE)
+                self.right_pwm = 2 * self.initial_wheel_pwm
+                self.left_pwm = self.initial_wheel_pwm                
+            elif self.count_in_stage2 < 3: 
+                self.count_in_stage2 += 1
+                self.right_pwm = 2 * self.initial_wheel_pwm
+                self.left_pwm = self.initial_wheel_pwm
             # 曲率太大, 右輪減速or左輪加速       
-            if (x - 1) ** 2 + (y - 0.25) ** 2 < 0.25 ** 2: # left_pwm++ or right_pwm--
-                self.right_pwm -= 2
+            elif (x - 1) ** 2 + (y - 0.25) ** 2 < 0.25 ** 2: # left_pwm++ or right_pwm--
+                self.right_pwm -= 3
             # 曲率太小, 右輪加速or左輪減速
             elif (x - 1) ** 2 + (y - 0.25) ** 2 > 0.25 ** 2: # left_pwm-- or right_pwm++
-                self.left_pwm += 2
+                self.left_pwm -= 3
             self.leftMotor.setSpeed(self.left_pwm)
             self.rightMotor.setSpeed(self.right_pwm)            
             self.leftMotor.run(self.FORWARD)
-            self.rightMotor.run(self.FORWARD)      
-            self.right_pwm += 1
-            self.left_pwm += 1                              
+            self.rightMotor.run(self.FORWARD)                         
         elif x > 0 and y > 0: # theta > pi, 走直線            
             if theta - pi > 0: # left_pwm++ or right_pwm--            
                 self.right_pwm -= 2
@@ -69,12 +77,12 @@ class Tracking:
             self.rightMotor.setSpeed(self.right_pwm)
             self.leftMotor.run(self.FORWARD)
             self.rightMotor.run(self.FORWARD)
-            self.right_pwm += 1
-            self.left_pwm += 1            
         else: # 停車
             self.leftMotor.run(self.RELEASE)
             self.rightMotor.run(self.RELEASE)
-
+        # 每次都回復初始值
+        self.right_pwm = self.initial_wheel_pwm
+        self.left_pwm = self.initial_wheel_pwm
             
 
         # stages: 1) straight line,
